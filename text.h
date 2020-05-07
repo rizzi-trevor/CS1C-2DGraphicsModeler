06202291a4d35df886d5a253fc0a964b6eaaa2c9
ctope <63033714+ctope@users.noreply.github.com>
2020-05-07 15:05:19 -0700

    Update text.h

--- text.h
+++ text.h
@@ -2,15 +2,42 @@
 #define TEXT_H
 
 #include "shape.h"
+#include <string>
 
 class Text: public Shape
 {
 public:
     Text();
-
+    Text();
     ~Text();
 
     virtual void draw(QPaintDevice* device);
+    virtual void move(int xCoord, int yCoord);
+    
+    int getCenterPointX();
+    int getCenterPointY();
+    int getLength();
+    int getWidth();
+    
+    void setCenterPoint(int xCoord, int yCoord);
+    void setLength(int lengthValue);
+    void setWidth(int widthValue);
+    void setTextString(string text);
+    void setTextAlignment(int alignment);
+    void setTextPointSize(int pointSize);
+    void setTextColor(string color);
+    void setFontFamily(string fontFamily);
+    void setStyle(string fontStyle);
+    void setWeight(string fontWeight);
+    
+private:
+    int fontAlignment;
+    QString textString;
+    QFont font;
+    QPoint startPoint;
+    int length;
+    int width;
+    
 };
 
 #endif // TEXT_H
