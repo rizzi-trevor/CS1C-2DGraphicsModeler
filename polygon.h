cffdd44c59d62d1a8a70dd5cb9d3a03c7e86067c
ctope <63033714+ctope@users.noreply.github.com>
2020-05-06 18:04:28 -0700

    polygon header file
    added rest of functions, let me know if I missed anything

--- polygon.h
+++ polygon.h
@@ -7,15 +7,22 @@ class Polygon: public Shape
 {
 public:
     Polygon();
+    Polygon(const QColor &color, const PenStyle &Pstyle, const PenCapStyle &PCstyle, const PenJoinStyle &PJstyle, const BrushStyle &Bstyle, int width, int id, int x, int y);
     ~Polygon();
-
+ 
     virtual void draw(QPaintDevice* device);
 
     void addPoint(int x, int y);
+    void initializeList();
+    void draw(QPaintDevice *device);
+    void move(int x, int y);
+    virtual int getPerimeter();
+    virtual double getArea();
 
 private:
     QPoint *pointList;
     int pointCount;
+    vector <QPoint> qPt;
 };
 
 #endif // POLYGON_H
