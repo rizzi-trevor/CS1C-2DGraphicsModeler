92020a847a99b81cd1d22b7fa342b4befe7f9ef7
hannahjo9 <63033047+hannahjo9@users.noreply.github.com>
2020-05-11 21:38:41 -0700

    Update vector.h

--- vector.h
+++ vector.h
@@ -13,19 +13,23 @@ namespace myVec{
         T *elem;
         int space;
     public:
+        //! Constructs Vector
         vector() : size_v{0}, elem{nullptr}, space{0} {}
 
+        //! Constructs Vector with params
         explicit vector(int s) : size_v{s}, elem{new T[s]}, space{s}
         {
             for (int i = 0; i< size_v; ++i)
                 elem[i] = 0;
         }
 
+        //! Constructs a vector as a copy of an existing vector
         vector(const vector &src) : size_v{src.size_v}, elem{new T[src.size_v]}, space{src.space}
         {
             copy(src.elem, src.elem + size_v, elem);
         }
 
+        //! Overloaded assignment operator to assign an existing vector's values to another vector
         vector &operator=(const vector &src)
         {
             T *p = new T[src.size_v];
@@ -36,11 +40,13 @@ namespace myVec{
             return *this;
         }
 
+        //! Deconstructs Vector
         ~vector()
         {
             delete[] elem;
         }
 
+        //! Gets an element from a specific spot in a vector
         T &operator[](int n)
         {
             return elem[n];
@@ -50,16 +56,19 @@ namespace myVec{
     /*const T &operator[](int n) {
         return elem[n];
     }*/
-
+    
+        //! Gets vector size
         int size() const
         {
             return size_v;
         }
 
+        //! Gets vector capacity (how many elements a vector has)
         int capacity() const {
             return space;
         }
 
+        //! Resizes the vector
         void resize(int newsize) {
             reserve(newsize);
             for(int i = size_v; i< newsize; ++i)
@@ -67,6 +76,7 @@ namespace myVec{
             size_v = newsize;
         }
 
+        //! Adds a new element to the end of the vector
         void push_back(T d)
         {
             if(space == 0)
@@ -78,6 +88,7 @@ namespace myVec{
             ++size_v;
         }
 
+        //! Reserves more space to the vector
         void reserve(int newalloc)
         {
             if ( newalloc <= space) return;
@@ -92,6 +103,7 @@ namespace myVec{
         using iterator = T*;
         using const_iterator = const T*;
 
+        //! Gets front element of vector
         iterator begin()
         {
             if (size_v ==0)
@@ -99,6 +111,7 @@ namespace myVec{
             return &elem[0];
         }
 
+        //! Gets front element of vector
         const_iterator begin() const
         {
             if (size_v == 0)
@@ -106,6 +119,7 @@ namespace myVec{
             return &elem[size_v];
         }
 
+        //! Gets the last element of the vector
         iterator end()
         {
             if (size_v == 0)
@@ -113,6 +127,7 @@ namespace myVec{
             return &elem[size_v];
         }
 
+        //! Gets the last element of the vector
         const_iterator end() const
         {
             if (size_v ==0)
@@ -120,6 +135,7 @@ namespace myVec{
             return &elem[size_v];
         }
 
+        //! Inserts an element into the vector
         iterator insert(iterator p, const T &val)
         {
             int index = p - begin();
@@ -129,6 +145,7 @@ namespace myVec{
             iterator pp = begin() + index;
         }
 
+        //! Erases an element from the vector
         iterator erase(iterator p)
         {
             iterator returnIt = p;
