98c1276e54f2e1b25346a50bafff48f103c22e81
hannahjo9 <63033047+hannahjo9@users.noreply.github.com>
2020-05-11 21:23:01 -0700

    Update text.h

--- text.h
+++ text.h
@@ -7,29 +7,49 @@
 class Text: public Shape
 {
 public:
-    Text();
 
+    //! Constructs Text
+    Text();
+    
+    //! Constructs Text with params
     Text(const QColor &bColor, const QColor &color, const PenStyle &Pstyle, const PenCapStyle &PCstyle, const PenJoinStyle &PJstyle, const BrushStyle &Bstyle, int width, int id, string alignment, string fontFam, string fontStyle, string weight, string text, int x, int y, int textL, int textW);
 
+    //! Deconstructs Text
     ~Text();
 
+    //!Draws the Text to the canvas
     virtual void draw(QPaintDevice* device);
+    //! Moves the location of the Text on the canvas
     virtual void move(int xCoord, int yCoord);
     
+    //! Gets the center X coordinate
     int getCenterPointX();
+    //! Gets the center y coordinate
     int getCenterPointY();
+    //! Gets the length of the Text
     int getLength();
+    //! Gets the width of the Text
     int getWidth();
     
+    //! Sets the center point of the text
     void setCenterPoint(int xCoord, int yCoord);
+    //! Sets the length of the text
     void setLength(int lengthValue);
+    //! Sets the width of the text
     void setWidth(int widthValue);
+    //! Sets the actual text being displayed
     void setTextString(string text);
+    //! Sets the text allignment
     void setTextAlignment(string alignment);
+    //! Sets the text size
     void setTextPointSize(int pointSize);
+    //!Sets the text color
     void setTextColor(string color);
+    //! Sets the text font
     void setFontFamily(string fontFamily);
+    //! Sets the text style (italics, normal, oblique, etc...)
     void setFontStyle(string fontStyle);
+    //! Sets the text weight (thin, light, bold, extra bold, etc...)
     void setFontWeight(string fontWeight);
     
 private:
