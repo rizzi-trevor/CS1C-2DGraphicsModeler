cf60e4b0f1e38b4086e448ebaca8bbeb856f5c22
hannahjo9 <63033047+hannahjo9@users.noreply.github.com>
2020-05-11 21:48:56 -0700

    Update viewer.h

--- viewer.h
+++ viewer.h
@@ -16,39 +16,57 @@ class viewer : public QMainWindow
     Q_OBJECT
 
 public:
+    //! Constructs Shape viewer
     explicit viewer(QWidget *parent = nullptr);
 
+    //! Constructs Shape viewer
     viewer(QWidget *parent, bool admin);
+    
+    //! Deconstructs Shape Viewer
     ~viewer();
 
+    //! Displays the shapes
     void displayVector();
 
+    //! Updates Screen
     void updateScreen();
 
+    //! Removes a shape
     void removeShape();
 
 public slots:
 
+    //! Adds a line
     void onAddLine();
 
+    //! Adds a polyline
     void onAddPolyline();
 
+    //! Adds a polygon
     void onAddPolygon();
 
+    //! Adds a rectangle
     void onAddRectangle();
 
+    //! Adds a square
     void onAddSquare();
 
+    //! Adds an ellipse
     void onAddEllipse();
 
+    //! Adds a circle
     void onAddCircle();
 
+    //! Adds text
     void onAddText();
 
+    //! Logs out of shape viewer and returns to main window
     void onLogout();
 
+    //! Removes a shape
     void onRemoveClick();
 
+    //! Edits an existing shape
     void onEditClick();
 
 
